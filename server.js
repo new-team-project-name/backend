e1872046552d09463461d0aee657e8d64ef48c08
SorenHopkins <Soren.hopkins@gmail.com>
2019-09-25 13:42:42 -0400

    Add usage of attendance & event routes to server
    @SorenHopkins & @ehaffey
    Required attendance & event routes in server and 'used' them

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const cors = require('cors')
 // require route files
 const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
+const eventRoutes = require('./app/routes/event_routes')
+const attendanceRoutes = require('./app/routes/attendance_routes')
 
 // require middleware
 const errorHandler = require('./lib/error_handler')
@@ -65,6 +67,8 @@ app.use(requestLogger)
 // register route files
 app.use(exampleRoutes)
 app.use(userRoutes)
+app.use(eventRoutes)
+app.use(attendanceRoutes)
 
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
